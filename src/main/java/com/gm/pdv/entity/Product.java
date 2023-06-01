a3a709f7d727f33e8e2397f928c1afc0dae3c356
giana medeiros <gianamedeiros.00510@gmail.com>
2023-06-01 20:04:09 -0300

    created entity User and product

---
+++ src/main/java/com/gm/pdv/entity/Product.java
@@ -0,0 +1,26 @@
+package com.gm.pdv.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "tb_product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(length = 100, nullable = false)
+    private String description;
+    @Column(length = 20, precision = 20, scale = 2, nullable = false)
+    private BigDecimal price;
+    @Column(nullable = false)
+    private int quantity;
+
+}
---
+++ src/main/java/com/gm/pdv/entity/User.java
@@ -0,0 +1,21 @@
+package com.gm.pdv.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "tb_user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(length = 100, nullable = false)
+    private String name;
+    private boolean isEnabled;
+}
