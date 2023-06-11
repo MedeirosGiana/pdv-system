9a141adec1da4288350cf3c2d8024f5c56aa8cc5
giana medeiros <gianamedeiros.00510@gmail.com>
2023-06-10 22:44:38 -0300

    Customizing error message for invalid token

--- src/main/java/com/gm/pdv/security/JwtAuthFilter.java
+++ src/main/java/com/gm/pdv/security/JwtAuthFilter.java
@@ -1,6 +1,11 @@
 package com.gm.pdv.security;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gm.pdv.dto.ResponseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -29,24 +34,34 @@ public class JwtAuthFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
+        try {
+            String authorization = request.getHeader("Authorization");
 
-        String authorization = request.getHeader("Authorization");
+            if (authorization != null && authorization.startsWith("Bearer")) {
+                String token = authorization.split(" ")[1];
+                String username = jwtService.getUsername(token);
 
-        if (authorization != null && authorization.startsWith("Bearer")){
-            String token = authorization.split(" ")[1];
-            String username = jwtService.getUsername(token);
+                UserDetails userDetails = customUserDetailsService.loadUserByUsername(username);
 
-            UserDetails userDetails = customUserDetailsService.loadUserByUsername(username);
+                //cria um método que será inserido no contexto do spring security
+                UsernamePasswordAuthenticationToken userCtx =
+                        new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
 
-            //cria um método que será inserido no contexto do spring security
-            UsernamePasswordAuthenticationToken userCtx =
-                    new UsernamePasswordAuthenticationToken(userDetails,null, userDetails.getAuthorities());
+                //configurando o spring security como uma autenticação web
 
-            //configurando o spring security como uma autenticação web
-
-            userCtx.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-            SecurityContextHolder.getContext().setAuthentication(userCtx);
+                userCtx.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(userCtx);
+            }
+            filterChain.doFilter(request, response);
+        }catch (RuntimeException error){
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            response.getWriter().write(convertObjectToJson(new ResponseDTO("Token inválido!")));
         }
-        filterChain.doFilter(request,response);
+    }
+
+    private String convertObjectToJson(ResponseDTO responseDTO) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.writeValueAsString(responseDTO);
     }
 }
